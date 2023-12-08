db0cc6ab30d0b85aa7e248f15e8d646de3cc958c
DangerousDK <dpathakstar@gmail.com>
2023-12-08 18:12:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ladyshield/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ladyshield
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// Import statements
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Add your Kotlin code here, such as initializing UI components or handling user interactions
+    }
+}
